d57d8a6a1b122ecb58add8ca849021877be7652b
Antoine P <pigny.antoine@gmail.com>
2023-05-05 07:40:17 +0200

    added arrow to dropdown, next : refacto project structureand start rental page

--- src/components/layout/dropdown/Dropdown.jsx
+++ src/components/layout/dropdown/Dropdown.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import './Dropdown.scss'
+import { ReactComponent as Arrow } from '../../assets/arrow.svg'
 
 export default function Dropdown({ title, description }) {
    return (
       <details className='dropdown'>
-         <summary>{title}</summary>
+         <summary>
+            {title}
+            <Arrow />
+         </summary>
          <div>{description && <p>{description}</p>}</div>
       </details>
    )
