63446c6c1c435b380090a73dff486dea8426eca2
Antoine P <pigny.antoine@gmail.com>
2023-05-18 17:32:19 +0200

    changed import in root index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
+import './index.scss'
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import { App, Home, ErrorPage, About, Rental, rentalLoader } from 'modules/routes'
 
